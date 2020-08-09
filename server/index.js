b2b704e65ddac20b5c75a9706ae01df4f9073585
jusheen <jusheen9@gmail.com>
2020-08-09 15:33:55 -0700

    started up server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
 // import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = 3000;
 
 // middleware
+app.use( bodyParser.json() );
+app.use(express.static(path.join(__dirname, '../client/public')));
 
 // routes
+app.get('/products', function (req, res) {
+  res.send('GET request to products')
+})
 
 // start the server
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log('Failed to connect to server.')
+  } else {
+    console.log('Server listening on Port:', PORT);
+  }
+})
