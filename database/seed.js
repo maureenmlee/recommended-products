b372c92573ed9cbce53c5cfb3a97436f4a795e0f
jusheen <jusheen9@gmail.com>
2020-08-08 18:58:44 -0700

    done with testing database script

--- database/seed.js
+++ database/seed.js
@@ -74,46 +74,18 @@ var seed = (numOfEntries) => {
             })
           )
         }
+        Product.create(products, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully seeded the database.")
+          }
+        });
       }
     }
   });
 }
 
+// clear database before adding new products
+// await Product.remove()
 seed();
-// console.log()
-
-
-// export const seedUsers = async () => {
-// 	try {
-// 		/** check if already populated */
-// 		const usersCollection = await User.find()
-// 		if (usersCollection.length > 1) {
-// 			return
-// 		}
-// 		/** quantity to be generated */
-// 		const quantity = 10
-// 		/** empty array to store new data */
-// 		let users = []
-// 		for (let i = 0; i < quantity; i++) {
-// 			users.push(
-// 				new User({
-// 					email: faker.internet.email(),
-// 					password: faker.internet.password(),
-// 					username: faker.internet.userName()(),
-// 					url: faker.internet.url(),
-// 					photoURL: faker.internet.avatar(),
-// 					bio: faker.lorem.sentence()
-// 				})
-// 			)
-// 		}
-// 		/** little housekeeping before adding new users */
-// 		await User.remove()
-// 		/** create new database entry for every user in the array */
-// 		users.forEach(user => {
-// 			User.create(user)
-// 		})
-// 		console.log('Users Collection has been Populated!')
-// 	} catch (error) {
-// 		console.log(error)
-// 	}
-// }
