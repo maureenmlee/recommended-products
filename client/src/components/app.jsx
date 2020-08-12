f38a2e84e3ae6d743800706abfe05325970613e7
jusheen <jusheen9@gmail.com>
2020-08-11 23:33:23 -0700

    3/4 tests working so far. trying to write a test for fetching data

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import RecommendedProducts from './recommendedProducts.jsx';
 
 const axios = require('axios');
+// productId: window.location.pathname.match(/(\d+)/)[0],
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      productId: window.location.pathname.match(/(\d+)/) ? window.location.pathname.match(/(\d+)/)[0] : null,
       recommendedProductsData: []
     }
     this.getOneProduct = this.getOneProduct.bind(this);
@@ -18,7 +21,8 @@ class App extends React.Component {
   }
 
   getOneProduct() {
-    axios.get('/data/products')
+    let path = `/data/${this.state.productId}`
+    axios.get(path)
       .then((data) => {this.setState({recommendedProductsData: data.data[0].recommendedProducts})})
       .catch(console.log)
   }
--- test/App.test.js
+++ test/App.test.js
@@ -10,6 +10,7 @@ describe('<App/>', () => {
   let wrapper;
   beforeEach(() => {
     wrapper = shallow(<App />)
+    wrapper.setState({productId: 2});
   });
 
   // make sure App renders
@@ -20,6 +21,26 @@ describe('<App/>', () => {
   // make sure App contains RecommendedProducts component
   it('should have <RecommendedProducts /> as a subcomponent', () => {
     expect(wrapper.containsMatchingElement(<RecommendedProducts />)).toEqual(true);
+  })
+
+  // make sure App's states are behaving correctly
+  it('should have correct states', () => {
+
+    // check if productId was correctly set to 2
+    expect(wrapper.state('productId')).toBe(2);
+
+    // check if recommendedProductsData is currently empty
+    expect(wrapper.state('recommendedProductsData').length).toBe(0);
+
+  });
+
+  it('should retrieve data from db', () => {
+
+    // check if recommendedProductsData has correct data after receiving it from db
+    expect(wrapper.state('recommendedProductsData').length).toBe(0);
+    let appInstance = wrapper.instance();
+    appInstance.componentDidMount();
+    expect(wrapper.state('recommendedProductsData').length).not.toBe(0);
   })
 
 });
