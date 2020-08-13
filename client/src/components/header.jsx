124daf1cda686659a7dd476f66878cdc6a44b4d7
jusheen <jusheen9@gmail.com>
2020-08-12 19:38:47 -0700

    added margins to top and bottom. need to add the arrow now

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -3,31 +3,44 @@ import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 import { Grid, Row, Col } from '../gridRowCol.js';
 
-const HeaderStyle = styled.div`
-  margin: 40px;
-  border: 5px black;
+
+const HeaderGrid = styled(Grid)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1230px;
+  margin-left: 50px;
+  margin-right: 20px;
+  margin-bottom: 25px;
 `;
 
-const YouMay = styled.h3`
+const YouMay = styled.div`
   font-size: 32px;
   text-align: left;
-  letter-spacing: 0.5px;
+  letter-spacing: 0px;
   line-height: 36px;
-  font-family: Guardian-EgypTT, serif
+  font-family: Andale Mono, monospace
+  word-spacing: 0em;
+  font-weight: 100;
 `;
 
+const ShopMore = styled.div`
+  font-size: 18px;
+  text-align: left;
+  letter-spacing: normal;
+  line-height: 18px;
+  font-family: Optima, sans-serif;
+  font-weight: 100;
+`;
 
 const Header = () => (
-  <div>
-    <div>
-      <YouMay>
-        You may also like
-      </YouMay>
-    </div>
-    <div>
-      <a> Shop more similar items</a>
-    </div>
-  </div>
+  <HeaderGrid>
+    <YouMay>
+      You may also like
+    </YouMay>
+    <ShopMore>
+      Shop more similar items
+    </ShopMore>
+  </HeaderGrid>
 )
 
 export default Header;
