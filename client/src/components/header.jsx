a2dfa97efc6934c87782d4b18288a069d1298e89
jusheen <jusheen9@gmail.com>
2020-08-12 22:30:29 -0700

    aligned the arrow and shopmore component together

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -7,7 +7,7 @@ import { Grid, Row, Col } from '../gridRowCol.js';
 const HeaderGrid = styled(Grid)`
   display: grid;
   width: 1685px;
-  grid-template-columns: 9fr 1.4fr 10px;
+  grid-template-columns: 9fr 1.2fr 10px;
   grid-gap: 0px;
   margin-left: 50px;
   margin-right: 20px;
@@ -26,6 +26,7 @@ const YouMay = styled.div`
 `;
 
 const ShopMore = styled.div`
+  padding-top: 3px;
   font-size: 18px;
   text-align: right;
   letter-spacing: normal;
@@ -39,7 +40,7 @@ const Svg = styled.svg`
   background-color: red;
   padding: 0;
   margin: 0;
-  width: 20px;
+  width: 25px;
 `;
 
 
@@ -51,8 +52,7 @@ const Header = () => (
     <ShopMore>
       Shop more similar items
     </ShopMore>
-    <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" height="20"
-    width="50">
+    <Svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false">
       <path d="M17.3 12.7l.7-.7-.7-.7-4-4c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l2.3 2.3H7c-.6 0-1 .4-1 1s.4 1 1 1h7.2l-2.3 2.3c-.2.2-.3.4-.3.7 0 .6.4 1 1 1 .3 0 .5-.1.7-.3l4-4z">
       </path>
     </Svg>
