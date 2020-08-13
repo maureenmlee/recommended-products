361fcdce9ea9a224735d9d20f03029ac7f619699
jusheen <jusheen9@gmail.com>
2020-08-12 18:27:26 -0700

    learned how to extend styled components. going to refactor and play with grid

--- client/src/components/recommendedProducts.jsx
+++ client/src/components/recommendedProducts.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components'
 import { Grid, Row, Col } from '../gridRowCol.js';
 
+// build on top of styled components
+// A new component based on Col, but with some override styles
+const RecProCol = styled(Col)`
 
-
+`;
 
 let RecommendedProducts = (props) => (
-  <div>
-    {props.recommendedProductsData.map((product) => {
-      return <img src={product.image} />
-    })}
-  </div>
+  <Grid>
+    <Row>
+      {props.recommendedProductsData.map((product) => {
+        return (
+          <RecProCol size={1}>
+            <img src={product.image} />
+          </RecProCol>
+        )
+      })}
+    </Row>
+
+  </Grid>
+
 )
 
 export default RecommendedProducts;
 
+{/* <div>
+{props.recommendedProductsData.map((product) => {
+  return <img src={product.image} />
+})}
+</div> */}
--- server/index.js
+++ server/index.js
@@ -3,41 +3,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const db = require('../database/database.js');
+
 const app = express();
 const PORT = 3000;
 
-
 // middleware
-app.use('/products/:id', bodyParser.json() );
+app.use('/products/:id', bodyParser.json());
 app.use('/products/:id', express.static(path.join(__dirname, '../client/public')));
 
 // routes
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.status(200).send('This is the homepage.');
-})
+});
 
-app.get('/products', function (req, res) {
+app.get('/products', (req, res) => {
   res.status(200).send('This is the products page.');
-})
+});
 
 // type localhost3000/products/3 in the browser
 
 // retrieves the recommendedProducts data for the product with the currentId.
-app.get('/data/:id', function (req, res) {
+app.get('/data/:id', (req, res) => {
   db.getRecommendedProductsData(req.params.id, (err, data) => {
     if (err) {
       res.status(500).send('Could not retrieve recommended product data.');
     } else {
       res.status(200).send(data);
     }
-  })
-})
+  });
+});
 
 // start the server
 app.listen(PORT, (err) => {
   if (err) {
-    console.log('Failed to connect to server.')
+    console.log('Failed to connect to server.');
   } else {
     console.log('Server listening on Port:', PORT);
   }
-})
+});
